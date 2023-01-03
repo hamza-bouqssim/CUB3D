0ea07137b12da51286c7d852cf02fd6603f1c90a
Souhail <i.alaoui.souhail@gmail.com>
2023-01-03 20:12:38 +0100

    fixing wall collisions

--- movements/moves.c
+++ movements/moves.c
@@ -12,6 +12,22 @@ int	wall_check(t_data *data, double y, double x)
 	return (0);
 }
 
+int	collision_check(t_data *data, double move)
+{
+	double	col_x;
+	double	col_y;
+	double	col_x1;
+	double	col_y1;
+
+	col_x = data->player.x + cos(data->player.rot_angle - (data->rays.view_angle / 2)) * move;
+	col_y = data->player.y + sin(data->player.rot_angle - (data->rays.view_angle / 2)) * move;
+	col_x1 = data->player.x + cos(data->player.rot_angle + (data->rays.view_angle / 2)) * move;
+	col_y1 = data->player.y + sin(data->player.rot_angle + (data->rays.view_angle / 2)) * move;
+	if (wall_check(data, col_y, col_x) || wall_check(data, col_y1, col_x1))
+		return (1);
+	return (0);
+}
+
 void	w_move(t_data *data)
 {
 	double	move;
@@ -26,7 +42,7 @@ void	w_move(t_data *data)
 	y_wall = data->player.y + sin(data->player.rot_angle) * move;
 	x1_wall = ((data->Columns * data->scale) * x_wall) / data->width;
 	y1_wall = ((data->Rows * data->scale) * y_wall) / data->height;
-	if (wall_check(data, y_wall, x_wall))
+	if (wall_check(data, y_wall, x_wall) || collision_check(data, move))
 		return ;
 	data->player.x = x_wall;
 	data->player.y = y_wall;
